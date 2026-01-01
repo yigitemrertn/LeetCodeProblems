6650f1574bd6ceeff838d0e10f67964411179bd3
Yiğit Emre <94650441+yigitemrertn@users.noreply.github.com>
2026-01-01 22:18:59 +0300

    Time: 4 ms (20.87%) | Memory: 42.4 MB (37.93%) - LeetSync

--- 67-add-binary/add-binary.cs
+++ 67-add-binary/add-binary.cs
@@ -1,23 +1,31 @@
 public class Solution {
     public string AddBinary(string a, string b)
 {
-    int len = a.Length > b.Length ? a.Length : b.Length;
-    a = a.PadLeft(len, '0');
-    b = b.PadLeft(len, '0');
-    string fin = "";
-    int cin = 0;
-    for (int i = len-1; i >= 0; i--)
-    {
-        int inta = (int)char.GetNumericValue(a[i]);
-        int intb = (int)char.GetNumericValue(b[i]);
-        int sum = inta ^ intb ^ cin;
-        cin =  inta & intb | inta & cin| intb & cin;
-        fin = sum + fin;
-    }
-    if (cin != 0)
-    {
-        return cin + fin;
-    }
-    return fin;
+   int len = a.Length > b.Length ? a.Length : b.Length;
+a = a.PadLeft(len, '0');
+b = b.PadLeft(len, '0');
+var sb = new System.Text.StringBuilder();
+int cin = 0;
+for (int i = len-1; i >= 0; i--)
+{
+    int inta = (int)char.GetNumericValue(a[i]);
+    int intb = (int)char.GetNumericValue(b[i]);
+    int sum = inta ^ intb ^ cin;
+    cin =  inta & intb | inta & cin| intb & cin;
+    sb.Append(sum);
+}
+
+if (cin != 0)
+{
+    sb.Append(cin);
+}
+
+char[] result = new char[sb.Length];
+sb.CopyTo(0, result, 0, sb.Length);
+Array.Reverse(result);
+
+
+
+return new string(result);
 }
 }
